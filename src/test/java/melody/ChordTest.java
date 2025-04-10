201afc0fa693634ab7a2db697d450dd07577b8bd
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-10 22:28:32 +0700

    Added chord creation test

---
+++ src/test/java/melody/ChordTest.java
@@ -0,0 +1,28 @@
+package melody;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ChordTest {
+    @Test
+    public void testCreateChord() {
+        // Проверяем создание аккорда C
+        Chord chordC = new Chord("C");
+        assertNotNull(chordC);
+
+        // Проверяем создание аккорда Am
+        Chord chordAm = new Chord("Am");
+        assertNotNull(chordAm);
+
+        // Проверяем создание аккорда G
+        Chord chordG = new Chord("G");
+        assertNotNull(chordG);
+
+        // Проверяем создание аккорда F
+        Chord chordF = new Chord("F");
+        assertNotNull(chordF);
+    }
+
+}
