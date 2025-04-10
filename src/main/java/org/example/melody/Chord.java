fcb4f9d94c4497650aa0ddcfbaf6df567251b7fa
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-10 22:59:15 +0700

    Added getNotes method. testChordC & testUnknownChordThrowsException - Success

--- src/main/java/org/example/melody/Chord.java
+++ src/main/java/org/example/melody/Chord.java
@@ -1,7 +1,16 @@
 package org.example.melody;
 
+import java.util.List;
+
 public class Chord {
     private final String name;
 
     public Chord(String name) {this.name = name;}
+
+    public List<String> getNotes() {
+        if (name.equals("C")) {
+            return List.of("C", "E", "G");
+        }
+        throw new IllegalArgumentException("Unknown chord: " + name);
+    }
 }
