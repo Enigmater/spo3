affdfc1f032b1e108b58c50c253012e41cd057a4
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-10 23:45:42 +0700

    Added parse multiple chords test - Success

--- src/test/java/InputParserTest.java
+++ src/test/java/InputParserTest.java
@@ -15,4 +15,14 @@ public class InputParserTest {
         assertEquals("The chord should be C", "C", chords.get(0).getName());
     }
 
+    @Test
+    public void testParseMultipleChords() {
+        String input = "C Am G F";
+        List<Chord> chords = InputParser.parse(input);
+        assertEquals( "Should parse four chords", 4, chords.size());
+        assertEquals("The first chord should be C", "C", chords.get(0).getName());
+        assertEquals("The second chord should be Am", "Am", chords.get(1).getName());
+        assertEquals("The third chord should be G", "G", chords.get(2).getName());
+        assertEquals("The fourth chord should be F", "F", chords.get(3).getName());
+    }
 }
