9ff69c5c26ba9e7913c5e36b2c138c0423f692da
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-10 23:15:48 +0700

    Added generate note from chord test

--- src/test/java/melody/MelodyGeneratorTest.java
+++ src/test/java/melody/MelodyGeneratorTest.java
@@ -1,14 +1,24 @@
 package melody;
 
+import org.example.melody.Chord;
 import org.example.melody.MelodyGenerator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class MelodyGeneratorTest {
     @Test
     public void testCreateMelodyGenerator() {
         MelodyGenerator melodyGenerator = new MelodyGenerator();
         assertNotNull(melodyGenerator);
+    }
+
+    @Test
+    public void testGenerateNoteFromChord() {
+        MelodyGenerator gen = new MelodyGenerator();
+        Chord chord = new Chord("C");
+        String note = gen.generateNote(chord);
+        assertTrue(chord.getNotes().contains(note));
     }
 }
