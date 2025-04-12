9ede327b645c26bc506e6f3ad556174144f315ba
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-12 15:49:19 +0700

    Added run method. testRunGeneratesExpectedLengthMelody & testRunWithInvalidChordThrowsException - Success

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import org.example.melody.Chord;
 import org.example.melody.MelodyGenerator;
+
+import java.util.List;
 
 public class Runner {
     private final MelodyGenerator generator;
 
     public Runner() {
         generator = new MelodyGenerator();
+    }
+
+    public String run(String input) {
+        List<Chord> chords = InputParser.parse(input);
+        List<String> melody = generator.generateMelody(chords);
+        return String.join(" ", melody);
     }
 }
