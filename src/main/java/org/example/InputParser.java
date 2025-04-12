5d07b2cdf8083a4ff21ab3fa5276bebc7c48a487
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-12 16:01:51 +0700

    Added javadoc for InputParser.parse method

--- src/main/java/org/example/InputParser.java
+++ src/main/java/org/example/InputParser.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputParser {
+    /**
+     * Парсит строку аккордов в список объектов Chord.
+     * @param input строка аккордов, разделённых пробелами
+     * @return список объектов Chord
+     */
     public static List<Chord> parse(String input) {
         String[] names = input.trim().split("\\s+");
         List<Chord> chords = new ArrayList<>();
