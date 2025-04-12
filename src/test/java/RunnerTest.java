f6b45a4e46ba0bb8e25f67c2ffde3c996430bb41
Enigmater <ivan.tihonov1999@mail.ru>
2025-04-12 15:42:56 +0700

    Added runner creation test

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class RunnerTest {
+    @Test
+    public void testCreateRunner() {
+        Runner melodyRunner = new Runner();
+        assertNotNull(melodyRunner);
+    }
+}
